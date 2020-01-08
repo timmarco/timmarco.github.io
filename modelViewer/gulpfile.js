a873ff932ed759d7f55be8292d39ccc2e9725621
Tim Marco <tim@timmarco.com>
2020-01-08 00:24:08 -0600

    Fixed node modules for modelViewer

--- modelViewer/gulpfile.js
+++ modelViewer/gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 
 gulp.task('js',() => {
-  return gulp.src(['app/js/**/*.js'])
+  return gulp.src(['node_modules/three/build/three.min.js','node_modules/three/examples/js/loaders/GLTFLoader.js','node_modules/three/examples/js/controls/OrbitControls.js','node_modules/d3/dist/d3.min.js','app/js/**/*.js'])
     .pipe(concat('all.js'))
     .pipe(gulp.dest('app/dist'));
 });
