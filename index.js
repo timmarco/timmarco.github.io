ad5a4ae21fadcc4bd1546500ec1f808455dc342b
Tim Marco <tim@timmarco.com>
2021-04-14 17:43:44 -0500

    Favicon added

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/",(request,response) => {
   return response.sendFile(__dirname + "/index.html");
 });
 
+app.get("/favicon.ico",(request,response) => {
+  return response.sendFile(__dirname + "/favicon.ico");
+});
+
 app.listen(8000,() => {
   console.log("LOCALHOST IS RUNNING");
 });
